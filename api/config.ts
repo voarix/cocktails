6d44b7f9afd22af4f6959cf78b616805befd5d17
Kachkynov Alikhan <t8t228@gmail.com>
2025-05-30 05:04:16 +0600

    [api] add right path to mongodb

--- api/config.ts
+++ api/config.ts
@@ -5,7 +5,7 @@ const rootPath = __dirname;
 const config = {
   rootPath,
   publicPath: path.join(rootPath, "public"),
-  db: "mongodb://localhost/lalafo",
+  db: "mongodb://localhost/cocktails",
 };
 
 export default config;

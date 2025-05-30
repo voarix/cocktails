e5fd6aac6364793598ce069479689ce1affaad63
Kachkynov Alikhan <t8t228@gmail.com>
2025-05-30 21:16:28 +0600

    [frontend] add user registration, login and login with Google, logout, header (navbar), redux persistor for storage accessToken and cookie for refreshToken

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,32 @@
 import "./App.css";
+import { Container, CssBaseline, Typography } from "@mui/material";
+import { ToastContainer } from "react-toastify";
+import AppToolbar from "./components/UI/AppToolbar/AppToolbar.tsx";
+import { Route, Routes } from "react-router-dom";
+import Register from "./features/users/Register.tsx";
+import Login from "./features/users/Login.tsx";
 
 const App = () => {
-
   return (
     <>
+      <CssBaseline />
+      <ToastContainer autoClose={500} />
+      <header>
+        <AppToolbar />
+      </header>
+      <main>
+        <Container maxWidth="xl">
+          <Routes>
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
 
+            <Route
+              path="*"
+              element={<Typography variant="h4">Not found page</Typography>}
+            />
+          </Routes>
+        </Container>
+      </main>
     </>
   );
 };
---
+++ frontend/src/components/UI/AppToolbar/UserMenu.tsx
@@ -0,0 +1,100 @@
+import React, { useState } from "react";
+import { Avatar, Button, Menu, MenuItem } from "@mui/material";
+import type { User } from "../../../types";
+import { Link, useNavigate } from "react-router-dom";
+import { useAppDispatch } from "../../../app/hooks.ts";
+import { logout } from "../../../features/users/usersThunks.ts";
+import { unsetUser } from "../../../features/users/usersSlice.ts";
+import { toast } from "react-toastify";
+import { apiUrl } from "../../../globalConstants.ts";
+
+interface Props {
+  user: User;
+}
+
+const UserMenu: React.FC<Props> = ({ user }) => {
+  const navigate = useNavigate();
+  const [userOptionsEl, setUserOptionsEl] = useState<HTMLElement | null>(null);
+  const dispatch = useAppDispatch();
+
+  const handeClick = (event: React.MouseEvent<HTMLElement>) => {
+    setUserOptionsEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setUserOptionsEl(null);
+  };
+
+  const handleLogout = async () => {
+    try {
+      await dispatch(logout());
+      dispatch(unsetUser());
+      navigate("/");
+      toast.success("Logout is successful");
+    } catch (e) {
+      toast.error("Logout is failed");
+      console.error(e);
+    }
+  };
+
+  const avatarUrl = (path: string) => {
+    if (!path) {
+      return "/default.jpg";
+    }
+
+    if (path.startsWith("http") || path.startsWith("https")) {
+      return path;
+    }
+
+    return `${apiUrl}/${path}`;
+  };
+
+  return (
+    <>
+      <Link
+        to="/my-cocktails"
+        style={{
+          color: "white",
+          textDecoration: "none",
+          marginLeft: "6px",
+          fontWeight: 500,
+        }}
+      >
+        My Cocktails
+      </Link>
+      <Link
+        to="/add-cocktail"
+        style={{
+          color: "white",
+          textDecoration: "none",
+          marginLeft: "6px",
+          fontWeight: 500,
+        }}
+      >
+        New Cocktail
+      </Link>
+      <Button onClick={handeClick} color="inherit">
+        Hello, {user.displayName}
+        {user.avatar && (
+          <Avatar
+            src={avatarUrl(user.avatar)}
+            alt={user.displayName}
+            sx={{ ml: 2 }}
+          />
+        )}
+      </Button>
+      <Menu
+        keepMounted
+        anchorEl={userOptionsEl}
+        open={!!userOptionsEl}
+        onClose={handleClose}
+      >
+        <MenuItem>
+          <Button onClick={handleLogout}>Log Out</Button>
+        </MenuItem>
+      </Menu>
+    </>
+  );
+};
+
+export default UserMenu;
